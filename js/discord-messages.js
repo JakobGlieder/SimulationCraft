e4bdff7122fbfb4fdcb116a0c29e4f61d06be525
JakobGlieder <58510986+JakobGlieder@users.noreply.github.com>
2023-04-04 00:36:43 +0200

    Update discord-messages.js

--- js/discord-messages.js
+++ js/discord-messages.js
@@ -1,30 +1,3 @@
-async function loadMessageData(file) {
-  const response = await fetch(`discord%20messages/${file}`);
-  const data = await response.json();
-  return data;
-}
-
-async function fetchMessageFiles() {
-  const repoOwner = 'JakobGlieder';
-  const repoName = 'SimulationCraft';
-  const apiResponse = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/discord%20messages`);
-  const files = await apiResponse.json();
-  return files.filter((file) => file.name.endsWith('.json')).map((file) => file.name);
-}
-
-async function displayMessages() {
-  const files = await fetchMessageFiles();
-  const messagesDiv = document.getElementById('messages');
-
-  for (const file of files) {
-    const messageData = await loadMessageData(file);
-    const button = document.createElement('button');
-    button.textContent = file.replace('.json', '');
-    button.onclick = () => sendMessage(messageData);
-    messagesDiv.appendChild(button);
-  }
-}
-
 async function sendMessage(messageDataArray) {
   const webhookUrl = document.getElementById('webhook-url').value;
 
@@ -33,40 +6,45 @@ async function sendMessage(messageDataArray) {
     return;
   }
 
-  const embeds = messageDataArray.map((messageData) => {
-    return {
-      author: {
-        name: messageData.author,
-        url: messageData.authorUrl,
-        icon_url: messageData.authorImageUrl,
-      },
-      title: messageData.title,
-      description: messageData.description,
-      image: {
-        url: messageData.imageUrl,
-      },
-    };
-  });
+  const chunks = [];
+  for (let i = 0; i < messageDataArray.length; i += 10) {
+    chunks.push(messageDataArray.slice(i, i + 10));
+  }
 
-  const data = { embeds };
-
-  try {
-    const response = await fetch(webhookUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
+  for (const chunk of chunks) {
+    const embeds = chunk.map((messageData) => {
+      return {
+        author: {
+          name: messageData.author,
+          url: messageData.authorUrl,
+          icon_url: messageData.authorImageUrl,
+        },
+        title: messageData.title,
+        description: messageData.description,
+        image: {
+          url: messageData.imageUrl,
+        },
+      };
     });
 
-    if (response.ok) {
-      console.log('Message sent successfully.');
-    } else {
-      console.error('Error sending message:', response.statusText);
+    const data = { embeds };
+
+    try {
+      const response = await fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        console.log('Message sent successfully.');
+      } else {
+        console.error('Error sending message:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error sending message:', error);
     }
-  } catch (error) {
-    console.error('Error sending message:', error);
   }
 }
-
-displayMessages();
